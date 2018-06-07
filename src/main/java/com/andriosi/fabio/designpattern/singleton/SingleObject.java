ed5f620c6f3d3db07718959bde1312c8ad92a5eb
Fabio Luis Andriosi <fabio.andriosi@gmail.com>
2018-06-07 11:33:29 -0300

    Create Singleton pattern Signed-off-by:Fabio Luis Andriosi <fabio.andriosi@gmail.com>

---
+++ src/main/java/com/andriosi/fabio/designpattern/singleton/SingleObject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.andriosi.fabio.designpattern.singleton;
+
+/**
+ *
+ * @author rcpd2158
+ */
+public class SingleObject {
+    private static SingleObject instance = new SingleObject();
+    private SingleObject(){}
+    public static SingleObject getInstance(){
+        return instance;
+    }
+    
+    public String showMessage(){
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/andriosi/fabio/designpattern/factory/teste/TestSingleton.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.andriosi.fabio.designpattern.factory.teste;
+
+import com.andriosi.fabio.designpattern.singleton.SingleObject;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rcpd2158
+ */
+public class TestSingleton {
+    
+    public TestSingleton() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void message() {
+         assertEquals("Hello World!", SingleObject.getInstance().showMessage());
+     }
+}
