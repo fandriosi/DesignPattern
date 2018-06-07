fde105f4061a075b73d5574cbe0f57a1a1feb20c
Fabio Luis Andriosi <fabio.andriosi@gmail.com>
2018-06-07 11:07:54 -0300

    Test for pattern factory Signed-off-by:Fabio Luis Andriosi <fabio.andriosi@gmail.com> Signed-off-by:Fabio Luis Andriosi <fabio.andriosi@gmail.com> Signed-off-by:Fabio Luis Andriosi <fabio.andriosi@gmail.com> Signed-off-by:Fabio Luis Andriosi <fabio.andriosi@gmail.com>

---
+++ src/main/java/com/andriosi/fabio/designpattern/factory/ColorFactory.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.andriosi.fabio.designpattern.factory;
+
+/**
+ *
+ * @author rcpd2158
+ */
+public class ColorFactory extends AbstractFactory{
+
+    @Override
+    public Color getColor(Colors colors) {
+        if(colors == null){
+            return null;
+        }
+        if(colors == Colors.BLUE){
+            return new Blue();
+        }
+        if(colors == Colors.GREEN){
+            return new Green();
+        }
+        if(colors == Colors.RED){
+            return new Red();
+        }
+        return null;
+    }
+
+    @Override
+    public Shape getShape(Shapes shapes) {
+        return null;
+    }
+    
+}
---
+++ src/test/java/com/andriosi/fabio/designpattern/factory/teste/FactoryTeste.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.andriosi.fabio.designpattern.factory.teste;
+
+import com.andriosi.fabio.designpattern.factory.AbstractFactory;
+import com.andriosi.fabio.designpattern.factory.Choice;
+import com.andriosi.fabio.designpattern.factory.Color;
+import com.andriosi.fabio.designpattern.factory.Colors;
+import com.andriosi.fabio.designpattern.factory.FactoryProducer;
+import com.andriosi.fabio.designpattern.factory.Shape;
+import com.andriosi.fabio.designpattern.factory.Shapes;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rcpd2158
+ */
+public class FactoryTeste {
+    private final AbstractFactory sf;
+    private final AbstractFactory cf;
+    private Shape s;
+    private Color c;
+    public FactoryTeste() {
+        this.sf = FactoryProducer.getFactory(Choice.SHAPES);
+        this.cf = FactoryProducer.getFactory(Choice.COLORS);
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void Rectangle() {
+        s = sf.getShape(Shapes.RECTANGLE);
+        assertEquals("Inside Rectangle::draw() method.", s.draw());
+     }
+     @Test
+     public void Cicle() {
+        s = sf.getShape(Shapes.CICLE);
+        assertEquals("Inside Cicle::draw() method.", s.draw());
+     }
+     @Test
+     public void Square() {
+        s = sf.getShape(Shapes.SQUARE);
+        assertEquals("Inside Square::draw() method.", s.draw());
+     }
+     @Test
+     public void Red(){
+         c = cf.getColor(Colors.RED);
+        assertEquals("Inside Red:: fill() method", c.fill());
+     }
+     @Test
+     public void Blue(){
+         c = cf.getColor(Colors.BLUE);
+        assertEquals("Inside Blue:: fill() method", c.fill());
+     }
+     @Test
+     public void Green(){
+         c = cf.getColor(Colors.GREEN);
+        assertEquals("Inside Green:: fill() method", c.fill());
+     }
+}
