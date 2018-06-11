4140f65bbe7af4800ff11974d9ec01dd1bfc14a8
andriosi <andriosi@andriosi-VirtualBox>
2018-06-11 18:04:54 -0300

    Merge commit '71af06c8df5b8946a0d743351a4aa23de76b89f6'
    Conflicts:
    	src/test/java/com/andriosi/fabio/designpattern/factory/teste/BuilderTeste.java
    	src/test/java/com/andriosi/fabio/designpattern/factory/teste/FactoryTeste.java
    	src/test/java/com/andriosi/fabio/designpattern/factory/teste/TestSingleton.java

--- src/test/java/com/andriosi/fabio/designpattern/BuilderTeste.java
+++ src/test/java/com/andriosi/fabio/designpattern/BuilderTeste.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.andriosi.fabio.designpattern.teste;
+package com.andriosi.fabio.designpattern;
 
 import com.andriosi.fabio.designpattern.builder.Burger;
 import com.andriosi.fabio.designpattern.builder.Burgers;
--- src/test/java/com/andriosi/fabio/designpattern/PrototypeTeste.java
+++ src/test/java/com/andriosi/fabio/designpattern/PrototypeTeste.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.andriosi.fabio.designpattern.teste;
+package com.andriosi.fabio.designpattern;
 
 import com.andriosi.fabio.designpattern.prototype.Shape;
 import com.andriosi.fabio.designpattern.prototype.ShapeCache;
